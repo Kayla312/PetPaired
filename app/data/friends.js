793c47ec81f906c315ff196d23910cd0b6683c56
The Singer <kayla312@harbornet.com>
2018-11-13 12:38:03 -0800

    built a friends array (only three people, but gotta make it work)

---
+++ app/data/friends.js
@@ -0,0 +1,53 @@
+var friends = [
+    {
+      name: "Cinq",
+      photo:"http://ldywgilfmw17e67w3vjk9exe.wpengine.netdna-cdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
+      scores: [
+        "5",
+        "5",
+        "5",
+        "5",
+        "5",
+        "5",
+        "5",
+        "5",
+        "5",
+        "5"
+      ]
+    },
+    {
+      name: "Thackery Binx",
+      photo: "https://i.pinimg.com/originals/d6/65/be/d665be4c7e95c0ef01b6ca9beea7dc53.jpg",
+      scores: [
+        "5",
+        "5",
+        "1",
+        "3",
+        "5",
+        "1",
+        "1",
+        "5",
+        "3",
+        "3"
+      ]
+    },
+    {
+      name: "Dug Yeller",
+      photo: "https://3milliondogs.com/blog-assets-two/2015/03/013.jpg",
+      scores: [
+        "3",
+        "5",
+        "3",
+        "5",
+        "1",
+        "2",
+        "5",
+        "5",
+        "3",
+        "5"
+      ]
+    }];
+  
+  // Here we export the array. This makes it accessible to other files using require.
+  module.exports = friends;
+  
